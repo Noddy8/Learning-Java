aea3936f79a4f1880fb5936b00dc0d4628b167a7
Noddy8 <aamirpatel029@gmail.com>
2026-07-14 21:15:44 +0530

    Ex1 Compeleted

--- 1.CoreJava/Ex1/_30Thirty.java
+++ 1.CoreJava/Ex1/_30Thirty.java
@@ -8,10 +8,10 @@
 3 3 */
 public class _30Thirty {
     public static void main(String[] args) {
-        for(int i=1;i<=3;i++){
-            for(int j=1;j<=3;j++){
-                if(i==2 && j==2){
-                    continue; 
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                if (i == 2 && j == 2 || i == 2 && j == 3) {
+                    continue;
                 }
                 System.out.println(i + " " + j);
             }
